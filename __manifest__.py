3b2a1349892335291c55831295c8753425c8c477
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-05-05 08:22:57 -0300

    Changed to 19

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Payment',
-    'version': '17.0.0.1',
+    'version': '19.0.0.1',
 
     # any module necessary for this one to work correctly
     "external_dependencies": {
